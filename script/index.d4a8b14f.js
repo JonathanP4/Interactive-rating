245aaf93b458522d1d2cce03ebe76d5d82c23dfc
Jonathan <jonathanpetersen2016@gmail.com>
2023-01-31 23:55:40 -0300

    final result

--- script/index.d4a8b14f.js
+++ script/index.d4a8b14f.js
@@ -6,38 +6,45 @@ const thanksTitle = document.querySelector(".thank__you--title");
 const thanksTxt = document.querySelector(".thanks--text");
 //fix 100vh on mobile by setting height to the window's innerHeight
 document.body.style.height = `${window.innerHeight}px`;
-const checkHeight = ()=>{
+const checkHeight = () => {
     document.body.style.height = `${window.innerHeight}px`;
 };
 window.addEventListener("resize", checkHeight);
-const removeFromSiblings = ()=>ratingNumbers.forEach((el)=>el.classList.remove("active-rating"));
-const updaterating = (element)=>{
+const removeFromSiblings = () => ratingNumbers.forEach((el) => el.classList.remove("active-rating"));
+const updaterating = (element) => {
     rating.textContent = element.textContent;
     console.log(+rating.textContent);
-    if (+rating.textContent === 5) thanksTitle.textContent = "Thank you so much!";
-    if (+rating.textContent <= 2) {
+    if (+rating.textContent === 5) {
+        thanksTitle.textContent = "Thank you so much!";
+    }
+    else if (+rating.textContent <= 2) {
         thanksTitle.textContent = "We're sorry...";
         thanksTxt.textContent = `Please let us know how we could improve our service. All feedback is appreciated
       to help us improve our offering`;
+    } else {
+        thanksTitle.textContent = "Thank you!";
+        thanksTxt.textContent =
+            `We appreciate you taking the time to give a rating. If you ever need more support,
+        don't hesitate to get in touch!`;
     }
 };
-const checkRating = ()=>{
-    const checkSelection = Array.from(ratingNumbers).some((el)=>el.classList.contains("active-rating"));
+const checkRating = () => {
+    const checkSelection = Array.from(ratingNumbers).some((el) => el.classList.contains("active-rating"));
     //Only show "thank you" 'dialog' if the user slected a rating
     if (checkSelection) {
         containers[0].classList.add("d-none");
         containers[1].classList.remove("d-none");
     }
 };
-ratingNumbers.forEach((el)=>{
+ratingNumbers.forEach((el) => {
     //Desktop event handler
-    el.addEventListener("click", function() {
+    el.addEventListener("click", function () {
         removeFromSiblings();
         updaterating(el);
         this.classList.add("active-rating");
     });
     //Mobile event handler
-    el.addEventListener("touchend", function() {
+    el.addEventListener("touchend", function () {
         removeFromSiblings();
         updaterating(el);
         this.classList.add("active-rating");
